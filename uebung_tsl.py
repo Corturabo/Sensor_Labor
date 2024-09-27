cdf72100560088aa68c3471a1a053688e2258b4b
Corturabo <schwaigerf748@gmail.com>
2024-09-27 11:17:01 +0200

    Update uebung_tsl.py

--- uebung_tsl.py
+++ uebung_tsl.py
@@ -3,8 +3,8 @@ import board
 import adafruit_tsl2591
 import csv
 
-# Create sensor object, communicating over the board's default I2C bus
-i2c = board.I2C()  # uses board.SCL and board.SDA
+
+i2c = board.I2C()  
 
 
 sensor = adafruit_tsl2591.TSL2591(i2c)
@@ -30,13 +30,13 @@ with open('tsl2591_data.csv', 'a', newline='') as csvfile:
             print(f"Visible light: {visible}")
             print(f"Full spectrum (IR + visible) light: {full_spectrum}")
 
-            # Only write to CSV if values are within the valid range
+            
             if lux <= 100000 and infrared <= 100000:
                 with open('tsl2591_data.csv', 'a', newline='') as csvfile:
                     writer = csv.writer(csvfile)
                     writer.writerow([lux, infrared, visible, full_spectrum])
 
-            # Wait for 1 second before next reading
+        
             time.sleep(1.0)
 
         except Exception:
